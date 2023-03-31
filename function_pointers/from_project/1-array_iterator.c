0314016c67373e32107adf94e802f035c1ffde67
Adetops <heritagediwamu@gmail.com>
2023-03-31 08:06:28 -0700

    Function that prints a name

---
+++ function_pointers/from_project/1-array_iterator.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given as a
+ * parameter on each element of an array.
+ * @array: array to consider
+ * @size: number of elements in the array including the null byte
+ * @action: pointer to a function that takes int as argument and return nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	
+}
