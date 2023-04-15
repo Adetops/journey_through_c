67790a282a5f561fcfb357bd13995c241a08fea1
Adetops <heritagediwamu@gmail.com>
2023-04-15 04:25:16 -0700

    Main function executed last

---
+++ singly_linked_lists/project/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * printBeforeMain - function that prints a string before main is executed.
+ *
+ * Return: nothing.
+ */
+void printBeforeMain(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
