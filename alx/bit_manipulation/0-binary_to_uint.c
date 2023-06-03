a02514e688c2e0b05d99171cfec6f4052160a574
Adetops <heritagediwamu@gmail.com>
2023-06-03 10:41:36 -0700

    not working

--- alx/bit_manipulation/0-binary_to_uint.c
+++ alx/bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,7 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
@@ -9,5 +12,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int res = 0;
+	int i, n = strlen(b) - 1;
+77
+	if (b == NULL)
+		return (0);
 	
+	for (i = 0; b[i] != '\0'; i++, n--)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+		res = b[i] << n;
+		printf("%d\n", res);
+	}
+	return (uint);
 }
